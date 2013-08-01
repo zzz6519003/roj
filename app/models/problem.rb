74b39f54d3d6d461be494b5b2821261d89df0a63
Richard Ma <richard.ma.19850509@gmail.com>
2013-08-01 17:53:41 +0800

    居中对齐form #9

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,6 @@
 class Problem < ActiveRecord::Base
+  validates :title, :time_limit, :memory_limit, :description, :input, :output, presence: true
+
   def getRate()
     ret = 0
     ret = self.accepted.to_f/self.submit.to_f*100 if (not self.accepted.nil?) and (not self.submit.nil?)
