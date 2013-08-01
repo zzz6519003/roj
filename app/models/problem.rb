c3e8efde33c7585c7bff70eb69c4c8cb0545385a
Richard Ma <richard.ma.19850509@gmail.com>
2013-08-01 18:58:57 +0800

    fix #8 添加problem类的validates，修改默认local为en

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,5 +1,11 @@
 class Problem < ActiveRecord::Base
+  attr_accessible :title, :time_limit, :memory_limit, :description, :input, :output, presence: true
+
   validates :title, :time_limit, :memory_limit, :description, :input, :output, presence: true
+  validates :time_limit, :memory_limit, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+  }
 
   def getRate()
     ret = 0
