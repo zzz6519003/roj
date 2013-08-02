443f56e29410d311ee02fe7a43f9294a422789c4
Richard Ma <richard.ma.19850509@gmail.com>
2013-08-02 23:34:42 +0800

    add problem tests. fixed #2

--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return zero when submit and accepted is zero" do
+    simpleProblem = problems(:simpleProblem)
+    simpleProblem.submit = 0
+    simpleProblem.accepted = 0
+
+    assert problems(:simpleProblem).getAcceptedRate == 0.to_f, "Not return zero"
+  end
+
+  test "should return the rate of accepted/sbumit" do
+    simpleProblem = problems(:simpleProblem)
+    simpleProblem.submit = 100
+    simpleProblem.accepted = 50
+
+    assert simpleProblem.getAcceptedRate == 50.to_f, "No return accepted / submit"
+  end
 end
